7669d5283ce474f70bcbaa3465a9cd2a621956ed
mikyYun <mkyun2714@gmail.com>
2022-11-19 14:11:17 -0800

    backend url change

--- aiChat/src/api/fetch.js
+++ aiChat/src/api/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const sending = (msg) => {
-  return axios.post("http://localhost:5001/chat", {msg})
+  return axios.post("https://chatwithai.herokuapp.com/", {msg})
 };
 
 export { sending };
