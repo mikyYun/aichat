69d54e089d590aed7d55565d848b14ba4a6a7a8d
mikyYun <mkyun2714@gmail.com>
2022-11-19 14:22:47 -0800

    add headers

--- aiChat/src/api/fetch.js
+++ aiChat/src/api/fetch.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const headers = {
+    "Content-Type": "application/json",
+    'Access-Control-Allow-Origin': '*',
+    }
+
 const sending = (msg) => {
-  return axios.post("https://chatwithai.herokuapp.com/", {msg})
+  return axios.post("https://chatwithai.herokuapp.com/", {msg}, {headers})
 };
 
 export { sending };
