b46b7a16969481b5e9a084bf015ae08460eff666
mikyYun <mkyun2714@gmail.com>
2022-11-19 22:16:39 -0800

    connect backend

--- aiChat/src/api/fetch.js
+++ aiChat/src/api/fetch.js
@@ -9,8 +9,8 @@ const headers = {
     }
 
 const sending = (msg) => {
-  return axios.post("http://localhost:5001/chat/", {msg}, {headers})
-  // return axios.post("https://apichatwithai.herokuapp.com/chat", {msg}, {headers})
+  // return axios.post("http://localhost:5001/chat/", {msg}, {headers})
+  return axios.post("https://apichatwithai.herokuapp.com/chat", {msg}, {headers})
 };
 
 export { sending };
